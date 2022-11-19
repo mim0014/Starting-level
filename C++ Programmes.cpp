e66abb0a6101be36d4c96d1ed73fff9209e79252
Fatema Akter Mim <99816718+mim0014@users.noreply.github.com>
2022-11-19 17:33:14 +0600

    Add files via upload

---
+++ C++ Programmes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float num1,num2,sum,average;
+cout<< "Enter two numbers:";
+cin>> num1;
+cin>> num2;
+sum = num1 + num2;
+average = sum/2;
+cout << "sum ="<<sum <<"\n";
+cout <<"Average ="<< average <<"\n";
+return 0;
+}
