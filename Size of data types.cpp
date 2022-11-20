269949996f58cb6cff9836b620437f5ba0192186
Fatema Akter Mim <99816718+mim0014@users.noreply.github.com>
2022-11-20 11:26:36 +0600

    Add files via upload

---
+++ Size of data types.cpp
@@ -0,0 +1,17 @@
+//size of datatypes
+#include<iostream> 
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"size of int"<<sizeof(a)<<endl;
+	float b;
+	cout<<"size of float"<<sizeof(b)<<endl;
+	char c;
+	cout<<"size of char"<<sizeof (c)<<endl;
+	bool d;
+	cout<<"size of bool"<<sizeof (d)<<endl;
+	
+	return 0;
+
+}
